ea086a6d9f63d2c771dc82180a8f26701da088f0
Saksham Shukla <saksham.shukla1209@gmail.com>
2025-11-12 11:43:22 +0530

    Added final settings and env config

--- Banquet/settings.py
+++ Banquet/settings.py
@@ -125,7 +125,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
 # Securely load credentials from environment
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
